dd0a911904bc66d6929022e1158f629472288fdb
Andrew Striano <andrewstriano@gmail.com>
2023-02-24 15:33:40 -0800

    Add symbol buttons

--- js/main.js
+++ js/main.js
@@ -2,6 +2,22 @@ const gameBoard = (() => {
   // creates empty array to hold game squares
   let gameBoard = [];
   let playerPiece = "X";
+    // create O button
+  const oButton = document.createElement("button");
+  oButton.classList.add('playerPieceButton');
+  oButton.addEventListener('click', () => {
+    playerPiece = "O";
+  });
+  oButton.innerText = "O";
+  // create X button
+  const xButton = document.createElement('button');
+  xButton.classList.add('playerPieceButton');
+  xButton.addEventListener('click', () => {
+    playerPiece = "X";
+  });
+  xButton.innerText = "X";
+  // add both buttons to the page
+  document.querySelector(".symbolButtonContainer").append(oButton, xButton)  
   // pushes empty string to create game board tiles
   for (let index = 0; index < 9; index++) {
     gameBoard.push(" ");
@@ -14,11 +30,12 @@ const gameBoard = (() => {
     tile.classList.add("boardTile");
     tile.dataset.count = i;
     i++;
-    tile.addEventListener("click", () => {
+    tile.addEventListener("click", function playPiece () {
       tile.innerText = playerPiece;
       gameBoard[tile.dataset.count] = playerPiece;
       console.log(gameBoard);
       //   checkWin();
+      tile.removeEventListener("click", playPiece); // to prevent playing the same square
     });
     document.querySelector(".gameBoardContainer").appendChild(tile);
     return { tile, gameBoard, playerPiece }; // return these for use in gamePlay Obj
@@ -26,5 +43,7 @@ const gameBoard = (() => {
 
   return { gameBoard };
 })();
+
+
 
 console.log(gameBoard.gameBoard);
