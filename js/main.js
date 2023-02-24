7249ba3b1ae6ff389bf4726bc3a1fc5c734215f3
andrewstriano <111657219+andrewstriano@users.noreply.github.com>
2023-02-24 14:31:34 -0800

    Add tile counter

--- js/main.js
+++ js/main.js
@@ -1,25 +1,30 @@
-const gameBoard = ( () => {
-    // creates empty array to hold game squares
-    let gameBoard = [];
-    let playerPiece = "X";
-    // pushes empty string to create game board tiles
-    for (let index = 0; index < 9; index++) {
-        gameBoard.push(" ");
-    };
+const gameBoard = (() => {
+  // creates empty array to hold game squares
+  let gameBoard = [];
+  let playerPiece = "X";
+  // pushes empty string to create game board tiles
+  for (let index = 0; index < 9; index++) {
+    gameBoard.push(" ");
+  }
 
-    // create DOM elements to match array
-    gameBoard.forEach(element => {
-        let tile = document.createElement("div");
-        tile.classList.add("boardTile");
-        tile.addEventListener("click", () => {
-            tile.innerText = playerPiece;
-        });
-        document.querySelector(".gameBoardContainer").appendChild(tile);
-        return {tile}
+  // create DOM elements to match array
+  let i = 0;
+  gameBoard.forEach((element) => {
+    let tile = document.createElement("div");
+    tile.classList.add("boardTile");
+    tile.dataset.count = i;
+    i++;
+    tile.addEventListener("click", () => {
+      tile.innerText = playerPiece;
+      gameBoard[tile.dataset.count] = playerPiece;
+      console.log(gameBoard);
+      //   checkWin();
     });
+    document.querySelector(".gameBoardContainer").appendChild(tile);
+    return { tile, gameBoard, playerPiece }; // return these for use in gamePlay Obj
+  });
 
-    
-    return {gameBoard};
+  return { gameBoard };
 })();
 
 console.log(gameBoard.gameBoard);
